7cbe407216fc5f77ac6adbb9e08be6bfa798df8f
Marcel Körtgen <marcel.koertgen@gmail.com>
2018-01-20 19:49:27 +0100

    Add cors

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 app.use(bodyParser.json()); // for parsing application/json
+app.use(cors());
 
 // Add server routes
 require('./init')(app);
