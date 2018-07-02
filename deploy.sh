8420133dc23f9b39208f30363ab3a9f783abe007
alex-kuck <alexander.kucksdorf@gmail.com>
2018-07-02 16:56:11 +0200

    Update docker deployment

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
-docker-compose build decorator
+DOCKER_TAG=${TRAVIS_TAG}
+IMAGE_NAME=awesomeinc/neo4j-decorator:${DOCKER_TAG}
+# VCS_REF=${TRAVIS_COMMIT}
+VCS_REF=`git rev-parse --short HEAD`
+docker build --build-arg VCS_REF=\
+ --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`\
+ --build-arg DOCKER_TAG="${DOCKER_TAG}"\
+ -t ${IMAGE_NAME} .
 echo "$DOCKER_PWD" | docker login -u "$DOCKER_USER" --password-stdin
-docker-compose push decorator
+docker push ${IMAGE_NAME}
