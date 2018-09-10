30cdf15a9dbaf2c2ff298a961c65a72f05aeec11
Daniel Pink <daniel.pink@schoenhofer.de>
2018-09-10 13:59:46 +0200

    (decorator) make timeout adjustable

--- app/index.js
+++ app/index.js
@@ -3,6 +3,16 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
+const TIMEOUT = parseInt(process.env.TIMEOUT) || 120000;
+
+app.use(function (req, res, next) {
+  res.setTimeout(TIMEOUT, function () {
+    console.log(`Request has timed out. Timeout is set to ${TIMEOUT}`);
+    res.send(408);
+  });
+  next();
+});
+
 app.use(bodyParser.json()); // for parsing application/json
 app.use(cors());
 
